702dd46d79bf5c97acf448c223794e9a8d69fa51
Balder Kjøs <56432762+BalderKj@users.noreply.github.com>
2022-01-21 17:30:14 +0100

    UI tracking of wood
    Fixed temporary UI for tracking of total wood.  as well as some unity settings stuff.

--- Assets/Scripts/ResourceController.cs
+++ Assets/Scripts/ResourceController.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ResourceController : MonoBehaviour
 {
-
+    
+    public TextMeshProUGUI WoodCountText;
     public int totalWoodCount;
+
 
     void Update()
     {
         totalResourceCount();
+        UpdateText();
+
     }
     public void totalResourceCount()
     {
@@ -23,5 +28,10 @@ public class ResourceController : MonoBehaviour
             //food count
         }
         totalWoodCount = totalWoodCountTemp;
+    }
+
+    void UpdateText()
+    {
+        WoodCountText.text =  "wood: " +totalWoodCount.ToString();
     }
 }
